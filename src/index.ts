932e4fd60cc27edeccdf842f409960bef047c17d
Tal Rasha <talrasha007@gmail.com>
2019-10-02 11:11:30 +0800

    minimize package size.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Web3Wrapper } from '@0x/web3-wrapper';
-import { networks } from '../build/contracts/AABoard.json';
+import { networks } from '../build/networks/AABoard.json';
 import { AABoardContract, AABoardEvents } from './generated-wrappers/AABoard';
 
 export { AABoardEvents };
